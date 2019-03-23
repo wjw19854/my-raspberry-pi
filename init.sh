7c966bd194db6856a9b1e8e1a09c39b9fbd7fed9
wjw19854 <wjw19854@163.com>
2019-03-23 09:09:57 +0800

    增加说明

--- init.sh
+++ init.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 set -x
+#开启SSH
 apt install ssh
 systemctl start ssh
 systemctl enable ssh
+
+# 远程桌面
 apt-get purge realvnc-vnc-server -y
 apt install xrdp 
+
+# 拼音输入法
 apt-get install fcitx -y
 apt-get install fcitx-googlepinyin -y
 apt-get install fcitx-module-cloudpinyin fcitx-sunpinyin -y
-#. <( wget -O - https://code.headmelted.com/installers/apt.sh )
-#apt-get install -t stretch -y code-oss --allow-unauthenticated
-# vscode 
+
+# miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-armv7l.sh
+bash Miniconda3-latest-Linux-armv7l.sh
+
+# Firefox
+apt install firefox-esr firefox-esr-l10n-zh-cn
+
+# vscode下载
 chromium-browser --new-window https://github.com/stevedesmond-ca/vscode-arm/releases
+
